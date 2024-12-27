c3225f747cbdd41ddf9dd483e63fc7db8f85b8d4
MicVillacarlos <micvillacarlos@gmail.com>
2024-12-28 01:54:17 +0800

    remove console log

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,22 +1,18 @@
 export const onInitialClientRender = () => {
   try {
-    console.log("onInitialClientRender triggered");
     setTimeout(() => {
       const loader = document.getElementById("___loader");
       const gatsbyRoot = document.getElementById("___gatsby");
 
       if (loader) {
-        console.log("Hiding loader");
         loader.style.display = "none"; // Ensure loader is hidden
       } else {
         console.warn("Loader not found");
       }
 
       if (gatsbyRoot) {
-        console.log("Showing main content");
         gatsbyRoot.style.display = "block"; // Ensure main content is visible
       } else {
-        console.warn("Gatsby root not found");
       }
     }, 1000); // Adjust timing if necessary
   } catch (error) {
